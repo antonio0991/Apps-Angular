d2ba40dc6592b6e37201728492c8f0c227251530
Antonio Henriques de França Terceiro <antoniomed1.an@gmail.com>
2020-08-12 00:13:22 -0300

    port 51506

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ res.sendFile(path.join(__dirname+'/dist/demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 51506, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
